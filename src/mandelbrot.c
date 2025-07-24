12b107635ce547cb4765824b6869ba0fa61a6970
Julia <fish@fishcat.fish>
2025-07-24 13:41:32 -0700

    Added progress bar

--- src/mandelbrot.c
+++ src/mandelbrot.c
@@ -1,77 +1,105 @@
 // mandelbrot.c
 #include "mandelbrot.h"
 
-inline double pointIteratePeriodic(double x0, double y0, int max, double delta) {
-    double x = 0, y = 0;
-    double xold = 0, yold = 0;
-    double iterations = 0, period = 0;
+inline double
+pointIteratePeriodic(double x0, double y0, int max, double delta)
+{
+	double x = 0, y = 0;
+	double xold = 0, yold = 0;
+	double iterations = 0, period = 0;
 
-    // margin of error for periodicity checking
-    const int period_len = 32;
-    const double margin = delta / (period_len * 1000);
+	// margin of error for periodicity checking
+	const int period_len = 32;
+	const double margin = delta / (period_len * 1000);
 
-    while ((x * x + y * y) <= (1 << 16) && iterations < max) {
-        double xtemp = x * x - y * y + x0;
-        y = 2 * x * y + y0;
-        x = xtemp;
-        iterations++;
+	while ((x * x + y * y) <= (1 << 16) && iterations < max) {
+		double xtemp = x * x - y * y + x0;
+		y = 2 * x * y + y0;
+		x = xtemp;
+		iterations++;
 
-        // break if we're repeating
-        if ((x <= xold + margin && x >= xold - margin)
-             && (y <= yold + margin && y >= yold - margin)) {
-            iterations = max;
-            break;
-        }
+		// break if we're repeating
+		if ((x <= xold + margin && x >= xold - margin) &&
+		    (y <= yold + margin && y >= yold - margin)) {
+			iterations = max;
+			break;
+		}
 
-        period++;   
-        if (period > period_len) {
-            period = 0;
-            xold = x;
-            yold = y;
-        }
-    }
+		period++;
+		if (period > period_len) {
+			period = 0;
+			xold = x;
+			yold = y;
+		}
+	}
 
-    return iterations;
+	return iterations;
 }
 
-void generateMandelbrot(gdImagePtr img, const MandelData *data) {
-    const double xLower = data->xCenter - data->delta;
-    const double xUpper = data->xCenter + data->delta;
+void
+generateMandelbrot(gdImagePtr img, const MandelData *data)
+{
+	const double xLower = data->xCenter - data->delta;
+	const double xUpper = data->xCenter + data->delta;
 
-    const double yLower = -(data->yCenter) - data->delta *
-        ((double)data->height / data->width);
-    const double yUpper = -(data->yCenter) + data->delta *
-        ((double)data->height / data->width);
+	const double yLower =
+	    -(data->yCenter) -
+	    data->delta * ((double)data->height / data->width);
+	const double yUpper =
+	    -(data->yCenter) +
+	    data->delta * ((double)data->height / data->width);
 
-    const double xDifference = xUpper - xLower;
-    const double yDifference = yUpper - yLower;
+	const double xDifference = xUpper - xLower;
+	const double yDifference = yUpper - yLower;
 
-    const double xStep = xDifference / data->width;
-    const double yStep = yDifference / data->height;
+	const double xStep = xDifference / data->width;
+	const double yStep = yDifference / data->height;
+
+	int progress = 0;
+	int barwidth = 40;
 
 #pragma omp parallel for schedule(dynamic)
-    for (int i = 0; i < data->width; i++) {
-        double x0 = xLower + i * xStep;
+	for (int i = 0; i < data->width; i++) {
+		double x0 = xLower + i * xStep;
 
-        for (int j = 0; j < data->height; j++) {
-            double y0 = yLower + j * yStep;
-            double iteration = pointIteratePeriodic(x0, y0, data->iterMax, data->delta);
-            hsv color;
+		for (int j = 0; j < data->height; j++) {
+			double y0 = yLower + j * yStep;
+			double iteration = pointIteratePeriodic(
+			    x0, y0, data->iterMax, data->delta);
+			hsv color;
 
-            if (data->colorIn && iteration == data->iterMax) {
-                color.h = 0;
-                color.s = 0;
-                color.v = 0;
-            } else {
-                color.h = (int)
-                    (powf((iteration / data->iterMax) * 360, data->huePower))
-                    % 360;
-                color.s = 1;
-                color.v = powf(iteration / data->iterMax, data->darkness);
-            }
+			if (data->colorIn && iteration == data->iterMax) {
+				color.h = 0;
+				color.s = 0;
+				color.v = 0;
+			} else {
+				color.h = (int)(powf(
+					      (iteration / data->iterMax) * 360,
+					      data->huePower)) %
+					  360;
+				color.s = 1;
+				color.v = powf(iteration / data->iterMax,
+					       data->darkness);
+			}
 
-            int toAdd = allocHexToImage(img, hsvToRgb(color));
-            gdImageSetPixel(img, i, j, toAdd);
-    }
-  }
+			int toAdd = allocHexToImage(img, hsvToRgb(color));
+			gdImageSetPixel(img, i, j, toAdd);
+		}
+#pragma omp atomic
+		progress++;
+		int pos = ((progress * 100) / (data->height)) * barwidth / 100;
+		printf("[");
+		for (int i = 0; i < barwidth; i++) {
+			if (i < pos) {
+				printf("=");
+			} else if (i == pos) {
+				printf(">");
+			} else {
+				printf(" ");
+			}
+		}
+		printf("] Progress: %d%%\r", (progress * 100) / data->height);
+		fflush(stdout);
+	}
+	printf("\n");
 }
--- src/mandelbrot.h
+++ src/mandelbrot.h
@@ -6,25 +6,27 @@
 #include <gd.h>
 #include <math.h>
 #include <omp.h>
+#include <stdio.h>
 
 #include "color.h"
 
 typedef struct {
-    int width;
-    int height;
-    double delta;
-    double xCenter;
-    double yCenter;
-    // maximum Iterations, as these increase, the background tends to become darker
-    double iterMax;
-    /* the power by which the hue is raised to, as this increases, the image becomes
-       more rainbow */
-    double huePower; 
-    /* the power by which the value is raised to, as this increases, the background
-       becomes darker */
-    double darkness;    
-    // color in the set black
-    bool colorIn;
+	int width;
+	int height;
+	double delta;
+	double xCenter;
+	double yCenter;
+	// maximum Iterations, as these increase, the background tends to become
+	// darker
+	double iterMax;
+	/* the power by which the hue is raised to, as this increases, the image
+	   becomes more rainbow */
+	double huePower;
+	/* the power by which the value is raised to, as this increases, the
+	   background becomes darker */
+	double darkness;
+	// color in the set black
+	bool colorIn;
 } MandelData;
 
 double pointIteratePeriodic(double x0, double y0, int max, double delta);
