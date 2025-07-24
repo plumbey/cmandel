3ba62d0ea3082b590117763b45f28a7383b379a6
Julia <fish@fishcat.fish>
2025-07-24 11:39:37 -0700

    Created flake.nix

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    { nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            gcc
+            gnumake
+            gd
+            zlib
+            bundler
+          ];
+        };
+      }
+    );
+}
