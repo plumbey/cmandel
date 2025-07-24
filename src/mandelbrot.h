a440d5381bcb1e2450d3c0373a7506a610299f6e
Julia <fish@fishcat.fish>
2025-07-24 13:41:32 -0700

    Added progress bar

--- src/mandelbrot.h
+++ src/mandelbrot.h
@@ -5,24 +5,24 @@
 #include <stdbool.h>
 
 typedef struct {
-    int width;
-    int height;
-    double delta;
-    double xCenter;
-    double yCenter;
-    // maximum Iterations, as these increase, the background tends to become
-    // darker
-    double iterMax;
-    /* the power by which the hue is raised to, as this increases, the image
-       becomes more rainbow */
-    double huePower;
-    /* the power by which the value is raised to, as this increases, the
-       background becomes darker */
-    double darkness;
-    // color in the set black
-    bool colorIn;
-    // output file path
-    char *output;
+	int width;
+	int height;
+	double delta;
+	double xCenter;
+	double yCenter;
+	// maximum Iterations, as these increase, the background tends to become
+	// darker
+	double iterMax;
+	/* the power by which the hue is raised to, as this increases, the image
+	   becomes more rainbow */
+	double huePower;
+	/* the power by which the value is raised to, as this increases, the
+	   background becomes darker */
+	double darkness;
+	// color in the set black
+	bool colorIn;
+	// output file path
+	char *output;
 } MandelData;
 
 double pointIteratePeriodic(double x0, double y0, int max, double delta);
